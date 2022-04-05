8e23b01485156c85214d43f5e5aa4f2550187676
Samaheh Loghmani <>
2022-04-04 23:23:49 -0400

    created function for getting detail by id

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".blog");
+
+const renderPosts = async () => {
+    let uri = "http://localhost:3000/posts";
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+
+    let template = "";
+    posts.forEach((post) => {
+        template += `
+        <div class="post">
+        <h2>${post.title}</h2>
+        <p><small>${post.likes} likes</small></p>
+        <p>${post.body.slice(0, 200)}</p>
+        <a href="./details.html?id=${post.id}">Reead more...</a>
+        </div>
+        `;
+    });
+    container.innerHTML = template;
+};
+
+window.addEventListener("DOMContentLoaded", () => renderPosts());
